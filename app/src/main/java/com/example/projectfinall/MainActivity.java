fc09b21905fd2a618b70e0c8391eeb8419723bf7
SS <ajajbeesa@gmail.com>
2024-06-02 20:35:51 +0300

    version 5.0

---
+++ app/src/main/java/com/example/projectfinall/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.projectfinall;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the window flags to make the activity fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
